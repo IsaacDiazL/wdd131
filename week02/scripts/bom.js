c126f8b542452d562f1e2313afbe9e8afceb3237
IsaacDiazL <diaz.sureste@gmail.com>
2025-09-10 14:13:41 -0700

    BOM file done

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,10 +1,24 @@
 const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
-const list = document.querySelector('_____');
-const li = document.createElement('li');
-const deleteButton = document.createElement('button');
-
-li.textContent = input.value;
-deleteButton.textContent = '❌';
-li.append(deleteButton);
-list.append(li);
+const list = document.querySelector('#list'); // you need to fill in the blank to reference the HTML element that is a unordered list element.
+        
+button.addEventListener('click', function () {
+    if (input.value.trim() !== '') {
+        const li = document.createElement('li');
+        const deleteButton = document.createElement('button');
+        li.textContent = input.value;
+        deleteButton.textContent = '❌';
+        li.append(deleteButton);
+        list.append(li);
+        input.value = '';
+        input.focus();
+        
+        deleteButton.addEventListener('click', function () {
+        list.removeChild(li);
+        input.focus();
+            });
+    }
+    else {
+        alert('Please enter a valid chapter');
+    }
+});

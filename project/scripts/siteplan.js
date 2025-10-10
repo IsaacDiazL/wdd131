937506445c8e6ba0979e892550bbd02c90f507fa
IsaacDiazL <diaz.sureste@gmail.com>
2025-10-10 00:13:06 -0700

    siteplan project updated, added home page

--- project/scripts/siteplan.js
+++ project/scripts/siteplan.js
@@ -4,4 +4,12 @@ displayYear.innerHTML = `© ${date.getFullYear()}`;
 
 const lastModified = document.querySelector("#lastModified");
 const modified =  new Date(document.lastModified);
-lastModified.innerHTML = `Last modified: ${modified.toLocaleString("en-US", {dateStyle: "short",timeStyle: "short"})}`;
+lastModified.innerHTML = `Last modified: ${modified.toLocaleString("en-US", { dateStyle: "short", timeStyle: "short" })}`;
+
+const menuButton = document.querySelector("#menu");
+const navigationBar = document.querySelector(".navigation");
+
+menuButton.addEventListener('click', () => {
+    navigationBar.classList.toggle('show')
+    menuButton.classList.toggle('show')
+})
